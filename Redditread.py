1c8d6c0da1c9ee60df9d2a5edeb7dd15cf689137
Ioan Loosley <legit.ioangogo@gmail.com>
2014-08-06 22:13:47 +0100

    Improved ui and changed user agent

--- Redditread.py
+++ Redditread.py
@@ -1,7 +1,7 @@
 import urllib2
 import os
 from bs4 import BeautifulSoup
-hdr = { 'User-Agent' : 'the python Text post reader /u/ioangogo using urllib2, espeak and BeautifulSoup4' }
+hdr = { 'User-Agent' : 'The python text post reader /u/ioanthecomputerguy using urllib2, espeak and BeautifulSoup4' }
 page = urllib2.Request(raw_input("Enter reddit url: "), headers=hdr)
 html = urllib2.urlopen(page).read()
 
@@ -13,7 +13,7 @@ print mydiv.text
 f = open('file.txt','w')
 f.write(mydiv.text.encode('utf-8'))
 f.close()
-opt = str(raw_input("1.Save as wav 2. Play out load: "))
+opt = str(raw_input("1.Save as wav 2. Play out loud: "))
 if opt == "1":
  os.popen("espeak -a 100 -w" + title[0].text + " -f file.txt")
 elif opt == "2":
